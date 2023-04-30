7d2ad7b6b77399e15233557ed62696b730d7dcc5
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2023-04-30 17:03:59 +0100

    add ids to span elements, reference in getSection object

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 const getSection = (() => {
     'use strict';
+
     const header = document.getElementById('header');
     const main = document.getElementById('main');
     const sidebar = document.getElementById('sidebar');
     const content = document.getElementById('content');
-    return { header, main, sidebar, content };
+
+    const home = document.getElementById('home');
+    const today = document.getElementById('today');
+    const week = document.getElementById('week');
+    const groups = document.getElementById('groups');
+    const notes = document.getElementById('notes');
+    return { 
+            header, 
+            main, 
+            sidebar, 
+            content,
+            home,
+            today,
+            week,
+            groups,
+            notes 
+           };
 })();
 
 export default getSection
 
 console.log(getSection);
-console.log('hello world');
