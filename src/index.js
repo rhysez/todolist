f295e986162d19a354a21d64903f4544cc193abe
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2023-04-30 16:56:37 +0100

    fix bug where getSection object was returning as undefined

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const getSection = (() => {
-    const header = document.getElementById('header')
-    const main = document.getElementById('main')
-    const sidebar = document.getElementById('sidebar')
-    const content = document.getElementById('content')
-    return { header, main, sidebar, content }
+    'use strict';
+    const header = document.getElementById('header');
+    const main = document.getElementById('main');
+    const sidebar = document.getElementById('sidebar');
+    const content = document.getElementById('content');
+    return { header, main, sidebar, content };
 })();
 
 export default getSection
 
-console.log('Hello World');
+console.log(getSection);
+console.log('hello world');
